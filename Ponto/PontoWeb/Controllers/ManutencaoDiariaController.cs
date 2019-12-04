447a70f1799a6b183c4cad8c9b8ae82b2015d494
Cristiane Paiva - Testes, Qualidade e Inovação - Employer <cristianepaiva@employer.com.br>
2019-12-04 19:09:21 +0000

    Reintegrada a Branch 81127 para a Main. Imported from Changeset 147963

--- Ponto/PontoWeb/Controllers/ManutencaoDiariaController.cs
+++ Ponto/PontoWeb/Controllers/ManutencaoDiariaController.cs
@@ -28,9 +28,6 @@ namespace PontoWeb.Controllers
             BLL.ConfiguracoesGerais bllConfiguracoes = new BLL.ConfiguracoesGerais(_usr.ConnectionString, _usr);
             Marcacao marc = new Marcacao();
 
-            marc.DataInicial = DateTime.Now.Date;
-            marc.DataFinal = DateTime.Now.Date;
-
             marc.Empresa = "0 | TODAS AS EMPRESAS";
             marc.Departamento = "0 | TODOS OS DEPARTAMENTOS";
             marc.Contrato = "0 | TODOS OS CONTRATOS";
