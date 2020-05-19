011c6ccd50ca1a20a75654a7c7c7f0683d1b27d1
Diego Herrera - Desenvolvimento - Employer <diegoherrera@employer.com.br>
2020-05-19 11:33:25 -0300

    #1861 #2873 Corrigida a exibição de mensagem de erro de campos obrigatórios

--- Ponto/Modelo/LancamentoCartaoPonto.cs
+++ Ponto/Modelo/LancamentoCartaoPonto.cs
@@ -11,16 +11,16 @@ namespace Modelo
         [Required]
         public string Funcionario { get; set; }
         public int IdFuncionario { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O campo Data Inicial é obrigatório.")]
         public string DataInicial { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O campo Data Final é obrigatório.")]
         public string DataFinal { get; set; }
         public List<LancamentoCartaoPontoRegistros> Regs { get; set; }
         public int QuantidadeRegistros { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O campo Justificativa é obrigatório.")]
         [Display(Name = "Justificativa")]
         public string DescJustificativa { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O campo Motivo é obrigatório.")]
         public string Motivo { get; set; }
         public int IdJustificativa { get; set; }
     }
