afa3707c6a06c8442638583a5c132f3a879f4fa6
Diego Herrera - Desenvolvimento - Employer <diegoherrera@employer.com.br>
2021-05-03 09:16:54 -0300

    Alterado o projeto SendMessagePontofopagWeb para pegar as variaveis de conexão do RabbitMQ pelo app.config ao invés de ficar fixo

--- Ponto/SendMessagePontoWeb/RabbitMqConsumer.cs
+++ Ponto/SendMessagePontoWeb/RabbitMqConsumer.cs
@@ -5,6 +5,7 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
@@ -24,40 +25,52 @@ namespace SendMessagePontoWeb
         {
             try
             {
-                var factory = new ConnectionFactory()
-                {
-                    HostName = "localhost"//, Port = 6969, UserName = "guest", Password = "guest"
-                };
-                using (var connection = factory.CreateConnection())
-                using (var channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "SendMessagePontoWeb",
-                                         durable: true,
-                                         exclusive: false,
-                                         autoDelete: false,
-                                         arguments: null);
+                string hostName = ConfigurationManager.AppSettings["HostName"];
+                string port = ConfigurationManager.AppSettings["Port"];
+                string userName = ConfigurationManager.AppSettings["Username"];
+                string password = ConfigurationManager.AppSettings["Password"];
+                string queueName = ConfigurationManager.AppSettings["QueueName"];
 
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
+                if (int.TryParse(port, out int portInt))
+                {
+                    var factory = new ConnectionFactory()
                     {
-                        var body = ea.Body.ToArray();
-                        var message = Encoding.UTF8.GetString(body);
-                        Console.WriteLine(" [x] Received {0}", message);
-
-
-                        PxyJobReturn job = JsonConvert.DeserializeObject<PxyJobReturn>(message);
-                        NotificationHub.ReportarJobProgresso(job);
-                        if (job.Progress == 100)
-                        {
-                            NotificationHub.ReportarJobCompleto(job);
-                        }
+                        HostName = hostName,
+                        Port = portInt,
+                        UserName = userName,
+                        Password = password
                     };
-                    channel.BasicConsume(queue: "SendMessagePontoWeb",
-                                         autoAck: true,
-                                         consumer: consumer);
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: queueName,
+                                             durable: true,
+                                             exclusive: false,
+                                             autoDelete: false,
+                                             arguments: null);
 
-                    Console.WriteLine(" Press [enter] to exit.");
-                    Console.ReadLine();
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.Received += (model, ea) =>
+                        {
+                            var body = ea.Body.ToArray();
+                            var message = Encoding.UTF8.GetString(body);
+                            Console.WriteLine(" [x] Received {0}", message);
+
+
+                            PxyJobReturn job = JsonConvert.DeserializeObject<PxyJobReturn>(message);
+                            NotificationHub.ReportarJobProgresso(job);
+                            if (job.Progress == 100)
+                            {
+                                NotificationHub.ReportarJobCompleto(job);
+                            }
+                        };
+                        channel.BasicConsume(queue: "SendMessagePontoWeb",
+                                             autoAck: true,
+                                             consumer: consumer);
+
+                        Console.WriteLine(" Press [enter] to exit.");
+                        Console.ReadLine();
+                    } 
                 }
             }
             catch (Exception)
