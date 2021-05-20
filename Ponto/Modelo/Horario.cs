6d423cedfe8a9195bdfbf8e793dc53354a28e90a
ArthurOratz <arthur_oratz@hotmail.com>
2021-05-20 10:22:26 -0300

    alteracao na mascara do campo Desc. Horas DSR do cadastro de Horario

--- Ponto/Modelo/Horario.cs
+++ Ponto/Modelo/Horario.cs
@@ -499,6 +499,7 @@ namespace Modelo
 
 
         [Display(Name = "Desc. Horas DSR")]
+        [StringLength(4, ErrorMessage = "Valor Inválido.")]
         [DataTableAttribute()]
         public decimal Descontohorasdsr { get; set; }
 
